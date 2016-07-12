98f79674f2c271b371623867be14f29453ee6786
Peter Lawler <relwalretep@gmail.com>
2016-07-12 17:04:50 +1000

    Remove stray whitespace

--- BeagleBrew/beaglebrew.py
+++ BeagleBrew/beaglebrew.py
@@ -19,7 +19,6 @@
 # WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR 
 # IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-
 from multiprocessing import Process, Pipe, Queue, current_process
 from Queue import Full
 from subprocess import Popen, PIPE, call
