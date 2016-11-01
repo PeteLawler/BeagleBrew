225475590028214426170ce227a55ae05503538c
Peter Lawler <relwalretep@gmail.com>
2016-11-01 17:14:42 +1100

    Fix output document

--- beaglebrew_setup.sh
+++ beaglebrew_setup.sh
@@ -158,10 +158,10 @@ fi
 echo "-----------------------------------"
 
 echo "Installing custom Dallas 1W overlay"
-wget --continue --output-document ${DOWNLOAD_LOCATION}/bb.org-overlays/src/arm/. \
+wget --continue --output-document ${DOWNLOAD_LOCATION}/bb.org-overlays/src/arm/PL-W1-P9.27-00A0.dts \
   https://raw.githubusercontent.com/PeteLawler/PL-BB-overlays/master/src/arm/PL-W1-P9.27-00A0.dts
 echo "Installing custom UART4 overlay"
-wget --continue --output-document ${DOWNLOAD_LOCATION}/bb.org-overlays/src/arm/. \
+wget --continue --output-document ${DOWNLOAD_LOCATION}/bb.org-overlays/src/arm/PL-UART4-00A0.dts \
   https://raw.githubusercontent.com/PeteLawler/PL-BB-overlays/master/src/arm/PL-UART4-00A0.dts
 echo "-----------------------------------"
 
